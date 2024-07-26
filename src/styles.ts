2889d4ab9b539aaab4a41aa2690a97b6fe033302
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-07-26 15:47:25 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components' //acesso a estilização do DOM
+
+const EstiloGlobal = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+}
+`
+
+export default EstiloGlobal
