c8d08b3478f3ecb615422916ceb37952ff82a36c
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-09-30 00:29:18 -0300

    centralizando img de GithubSecao

--- src/containers/Sobre/styles.ts
+++ src/containers/Sobre/styles.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 export const GithubSecao = styled.div`
   margin-top: 32px;
   margin-bottom: 64px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   img {
     height: 157px;
